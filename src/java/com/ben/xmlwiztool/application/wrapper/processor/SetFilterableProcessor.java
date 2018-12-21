ef5776d929b289a336743bad16ec4c6e68d6c77b
bensamazingsoft <ben.mouginot@gmail.com>
2018-12-21 13:31:15 +0100

    before removing buttons in stickers

--- src/java/com/ben/xmlwiztool/application/wrapper/processor/SetFilterableProcessor.java
+++ src/java/com/ben/xmlwiztool/application/wrapper/processor/SetFilterableProcessor.java
@@ -4,8 +4,6 @@ import com.ben.xmlwiztool.application.wrapper.ElementWrapper;
 
 public class SetFilterableProcessor implements WrapperProcessor {
 
-	// TODO checkbox must trigger hide all and true-tree-visibility toggle from
-	// non empty single element
 	@Override
 	public void processSingle(ElementWrapper wrapper) {
 
